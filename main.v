a18bb5818298eef46b3408f1fbfacf70538daedf
KhoaTruong <truongkhoa.1799@gmail.com>
2020-07-24 19:45:13 +0700

    132

---
+++ main.v
@@ -0,0 +1,124 @@
+module MAIN(clk, reset, flick, LED, current_state, current_index);
+input clk, reset, flick;
+output [15:0]LED;
+
+output reg [1:0]current_state;
+output reg [2:0]current_index;
+
+reg [1:0]next_state;
+reg [2:0]next_index;
+reg [4:0]next_LED;
+reg [4:0]current_LED;
+
+wire [2:0]flick_index;
+wire [4:0]flick_led;
+wire [1:0]flick_state;
+
+reg [4:0]max_array[5:0];
+reg [4:0]min_array[5:0];
+
+reg [4:0]flick_pos[1:0];
+reg [3:0]final_index;
+
+// reg pulse_flick;
+reg temp;
+reg temp_pulse;
+
+wire [4:0]max_value;
+wire [4:0]min_value;
+
+parameter IDLE = 2'b00, GO_UP = 2'b01, GO_DOWN = 2'b10, BEGIN = 2'b11;
+
+assign max_value = max_array[current_index];
+assign min_value = min_array[current_index];
+assign LED = (16'd1 << current_LED) - 1;
+
+assign flick_trigger = (current_state == IDLE || ( current_state == GO_DOWN & current_index != final_index & (current_LED == flick_pos[0] || current_LED == flick_pos[1]) )) ? flick : 0;
+
+assign flick_index = (current_state == IDLE) ? 2'd0 : ( current_index - 1 );
+assign flick_led = (current_state == IDLE) ? 5'd1 : ( current_LED + 1);
+assign flick_state = GO_UP;
+
+always@( * ) begin
+    if (reset) begin
+        case (current_state)
+            BEGIN: begin
+                max_array[0] = 5'd16;
+                max_array[1] = 5'd0;
+                max_array[2] = 5'd11;
+                max_array[3] = 5'd0;
+                max_array[4] = 5'd6;
+                max_array[5] = 5'd0; 
+
+                min_array[0] = 5'd0;
+                min_array[1] = 5'd6;
+                min_array[2] = 5'd0;
+                min_array[3] = 5'd0;
+                min_array[4] = 5'd0;
+                min_array[5] = 5'd0; 
+
+                flick_pos[0] = 5'd0;
+                flick_pos[1] = 5'd6;
+
+                final_index = 3'd6;
+
+                current_LED = 5'd0;
+                current_index = 3'd0;
+                current_state = IDLE;
+
+                next_LED = current_LED;
+                next_index = current_index;
+                next_state = current_state;
+            end
+            GO_UP: begin
+                if (current_LED < max_value) begin
+                    next_LED = current_LED + 1;
+                    next_index = current_index;
+                    next_state = current_state;
+                end
+                else if (current_LED == max_value) begin
+                    next_state = GO_DOWN;
+                    next_index = current_index + 1;
+                    next_LED = current_LED;
+                end
+            end 
+            GO_DOWN: begin
+                if (current_LED > min_value) begin
+                    next_LED = current_LED - 1;
+                    next_index = current_index;
+                    next_state = current_state;
+                end
+                else if (current_LED == min_value) begin
+                    next_state = GO_UP;
+                    next_index = current_index + 1;
+                    next_LED = current_LED;
+                end
+
+                if ( next_index == final_index ) begin 
+                    next_index = 3'd0;
+                    next_state = IDLE;
+                    next_LED = 5'd0;
+                end
+            end
+        endcase    
+    end
+end
+
+
+always@(posedge clk, negedge reset, posedge flick_trigger) begin
+    if (!reset) begin
+        current_state = BEGIN;
+    end
+    else if (clk) begin
+        current_state = next_state;
+        current_index = next_index;
+        current_LED = next_LED;
+    end
+    else if (flick_trigger) begin
+        current_state = flick_state;
+        current_index = flick_index;
+        current_LED = flick_led;
+    end
+end
+
+endmodule
---
+++ test_bench.v
@@ -0,0 +1,74 @@
+module test_bench();
+    reg clk, reset, flick;
+    wire [15:0]LED;
+    wire [1:0]state;
+    wire [2:0]current_index;
+    // wire pulse_flick;
+
+    initial begin
+        $display("----------------------------------------------------------------");
+        $display();
+    end
+
+    initial begin
+        #0 clk <= 1'b0;
+        #0 flick <= 1'b0;
+        #0 reset <= 1'b1;
+
+        #4 flick <= 1'b1;
+        #2 flick <= 1'b0;
+
+        #2 reset <= 1'b0;
+        #2 reset <= 1'b1;
+
+        #10 flick <= 1'b1;
+        #2 flick <= 1'b0;
+
+        #5 reset <= 1'b0;
+        #2 flick <= 1'b1;
+        #2 flick <= 1'b0;
+        #50 reset <= 1'b1;
+
+        #2 flick <= 1'b1;
+        #2 flick <= 1'b0;
+
+        #50 flick <= 1'b1;
+        #2 flick <= 1'b0;
+
+        #203 flick <= 1'b1;
+        #2 flick <= 1'b0;
+
+        #378 flick <= 1'b1;
+        #2 flick <= 1'b0;
+
+        #550 flick <= 1'b1;
+        #2 flick <= 1'b0;
+
+        #143 flick <= 1'b1;
+        #2 flick <= 1'b0;
+
+        #40 reset <= 1'b0;
+        #20 flick <= 1'b1;
+        #2 flick <= 1'b0;
+        #50 reset <= 1'b1;
+
+        #20 flick <= 1'b1;
+        #2 flick <= 1'b0;
+
+        #2000 $stop;
+    end
+
+    initial begin 
+        forever #5 clk = !clk; 
+    end
+
+    MAIN main(   
+        .clk(clk), 
+        .reset(reset), 
+        .flick(flick), 
+        .LED(LED),
+        .current_state(state),
+        .current_index(current_index)
+        // .pulse_flick(pulse_flick)
+    );
+endmodule
