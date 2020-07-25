b6ff8a88067cababeb5e9f112b3944a8ce34ecaa
truongkhoa.1799 <truongkhoa.1799@gmail.com.vn>
2020-07-25 23:17:22 +0700

    modify main and testbench

--- main.v
+++ main.v
@@ -1,9 +1,9 @@
-module Bound_Flasher(clk, reset, flick, LED);
+module Bound_Flasher(clk, reset, flick, LED, current_state, current_index);
 input clk, reset, flick;
 output [15:0]LED;
 
-reg [1:0]current_state;
-reg [2:0]current_index;
+output reg [1:0]current_state;
+output reg [2:0]current_index;
 
 reg [1:0]next_state;
 reg [2:0]next_index;
@@ -21,48 +21,46 @@ reg [4:0]min_array[5:0];
 reg [4:0]flick_pos[1:0];
 reg [3:0]final_index;
 
-// reg pulse_flick;
-//reg temp;
-//reg temp_pulse;
-
 wire [4:0]max_value;
 wire [4:0]min_value;
 
-parameter IDLE = 2'b00, GO_UP = 2'b01, GO_DOWN = 2'b10, BEGIN = 2'b11;
+parameter IDLE = 2'b00, GO_UP = 2'b01, GO_DOWN = 2'b10;
 
 assign max_value = max_array[current_index];
 assign min_value = min_array[current_index];
 assign LED = (16'd1 << current_LED) - 1;
 
-assign flick_trigger = (current_state == IDLE || ( current_state == GO_DOWN & current_index != final_index & (current_LED == flick_pos[0] || current_LED == flick_pos[1]) )) ? flick : 0;
+assign flick_trigger = ( ( current_state == IDLE && reset ) || ( current_state == GO_DOWN & current_index != ( final_index - 1 ) & (current_LED == flick_pos[0] || current_LED == flick_pos[1]) )) ? flick : 0;
 
-assign flick_index = (current_state == IDLE) ? 2'd0 : ( current_index - 1 );
-assign flick_led = (current_state == IDLE) ? 5'd1 : ( current_LED + 1);
+assign flick_index = ( current_state == IDLE ) ? 2'd0 : ( current_index - 1 );
+assign flick_led = ( current_state == IDLE ) ? 5'd1 : ( current_LED + 1 );
 assign flick_state = GO_UP;
+
+initial begin
+    max_array[0] = 5'd16;
+    max_array[1] = 5'd0;
+    max_array[2] = 5'd11;
+    max_array[3] = 5'd0;
+    max_array[4] = 5'd6;
+    max_array[5] = 5'd0; 
+
+    min_array[0] = 5'd0;
+    min_array[1] = 5'd6;
+    min_array[2] = 5'd0;
+    min_array[3] = 5'd0;
+    min_array[4] = 5'd0;
+    min_array[5] = 5'd0; 
+
+    flick_pos[0] = 5'd0;
+    flick_pos[1] = 5'd6;
+
+    final_index = 3'd6;
+end
 
 always@( * ) begin
     if (reset) begin
         case (current_state)
-            BEGIN: begin
-                max_array[0] = 5'd16;
-                max_array[1] = 5'd0;
-                max_array[2] = 5'd11;
-                max_array[3] = 5'd0;
-                max_array[4] = 5'd6;
-                max_array[5] = 5'd0; 
-
-                min_array[0] = 5'd0;
-                min_array[1] = 5'd6;
-                min_array[2] = 5'd0;
-                min_array[3] = 5'd0;
-                min_array[4] = 5'd0;
-                min_array[5] = 5'd0; 
-
-                flick_pos[0] = 5'd0;
-                flick_pos[1] = 5'd6;
-
-                final_index = 3'd6;
-
+            IDLE: begin
                 next_LED = 5'd0;
                 next_index = 3'd0;
                 next_state = IDLE;
@@ -104,19 +102,21 @@ end
 
 always@(posedge clk or negedge reset or posedge flick_trigger) begin
     if (!reset) begin
-        current_state <= BEGIN;
+        current_state <= IDLE;
         current_LED <= 5'd0;
         current_index <= 3'd0;
     end
-    else if (flick_trigger) begin
-        current_state <= flick_state;
-        current_index <= flick_index;
-        current_LED <= flick_led;
-    end
     else begin
-        current_state <= next_state;
-        current_index <= next_index;
-        current_LED <= next_LED;
+        if (flick_trigger) begin
+            current_LED <= flick_led;
+            current_state <= flick_state;
+            current_index <= flick_index;
+        end
+        else if (clk) begin
+            current_LED <= next_LED;
+            current_state <= next_state;
+            current_index <= next_index;
+        end
     end
 end
 
--- test_bench.v
+++ test_bench.v
@@ -1,9 +1,8 @@
 module test_bench();
     reg clk, reset, flick;
     wire [15:0]LED;
-    //wire [1:0]state;
-    //wire [2:0]current_index;
-    // wire pulse_flick;
+    wire [1:0]state;
+    wire [2:0]current_index;
     
     initial begin
        $recordfile("waves");
@@ -35,18 +34,25 @@ module test_bench();
         #50 reset <= 1'b1;
 
         #2 flick <= 1'b1;
-        #2 flick <= 1'b0;
+        #1 flick <= 1'b0;
 
         #50 flick <= 1'b1;
+        #1 flick <= 1'b0;
+        
+        #159 flick <= 1'b1;
+        #1 flick <= 1'b0;
+
+        
+        #47 flick <= 1'b1;
+        #1 flick <= 1'b0;
+
+        #375 flick <= 1'b1;
         #2 flick <= 1'b0;
 
-        #213 flick <= 1'b1;
+        #358 flick <= 1'b1;
         #2 flick <= 1'b0;
 
-        #378 flick <= 1'b1;
-        #2 flick <= 1'b0;
-
-        #550 flick <= 1'b1;
+        #190 flick <= 1'b1;
         #2 flick <= 1'b0;
 
         #143 flick <= 1'b1;
@@ -71,9 +77,9 @@ module test_bench();
         .clk(clk), 
         .reset(reset), 
         .flick(flick), 
-        .LED(LED)
-        //.current_state(state),
-        //.current_index(current_index)
+        .LED(LED),
+        .current_state(state),
+        .current_index(current_index)
         // .pulse_flick(pulse_flick)
     );
 endmodule
