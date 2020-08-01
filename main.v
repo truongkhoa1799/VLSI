f1dc52c3bdf1c41e274147c996788e7c56f3f87e
truongkhoa.1799 <truongkhoa.1799@gmail.com.vn>
2020-08-01 21:10:39 +0700

    remove begin state

--- main.v
+++ main.v
@@ -9,21 +9,38 @@ reg [1:0]next_state;
 reg [2:0]next_index;
 reg [15:0]next_LED;
 
-wire [2:0]flick_index;
-wire [4:0]flick_led;
-wire [1:0]flick_state;
 wire flick_trigger;
 
-reg [15:0]max_array[5:0];
-reg [15:0]min_array[5:0];
+wire [15:0]max_array[5:0];
+wire [15:0]min_array[5:0];
 
-reg [15:0]flick_pos[1:0];
-reg [3:0]final_index;
+wire [15:0]flick_pos[1:0];
+wire [3:0]final_index;
+
+assign max_array[0] = 16'b1111111111111111;
+assign max_array[1] = 16'd0;
+assign max_array[2] = 16'b0000011111111111;
+assign max_array[3] = 16'd0;
+assign max_array[4] = 16'b0000000000111111;
+assign max_array[5] = 16'd0; 
+
+assign min_array[0] = 16'd0;
+assign min_array[1] = 16'b0000000000111111;
+assign min_array[2] = 16'd0;
+assign min_array[3] = 16'd0;
+assign min_array[4] = 16'd0;
+assign min_array[5] = 16'd0; 
+
+assign flick_pos[0] = 16'd0;
+assign flick_pos[1] = 16'b0000000000111111;
+
+assign final_index = 3'd6;
+
 
 //-----------------------------------------------------------------------//
 //                     States used for this program                      //
 //-----------------------------------------------------------------------//
-parameter IDLE = 2'b00, GO_UP = 2'b01, GO_DOWN = 2'b10, BEGIN = 2'b11;
+parameter IDLE = 2'b00, GO_UP = 2'b01, GO_DOWN = 2'b10;
 
 //-----------------------------------------------------------------------//
 //                  Check flick trigger Combinational Logic              //
@@ -38,30 +55,6 @@ assign flick_trigger = ( ( current_state == IDLE && reset ) ||
 always@( * ) begin
     if (reset) begin
         case (current_state)
-            BEGIN: begin
-                max_array[0] = 16'b1111111111111111;
-                max_array[1] = 16'd0;
-                max_array[2] = 16'b0000011111111111;
-                max_array[3] = 16'd0;
-                max_array[4] = 16'b0000000000111111;
-                max_array[5] = 16'd0; 
-
-                min_array[0] = 16'd0;
-                min_array[1] = 16'b0000000000111111;
-                min_array[2] = 16'd0;
-                min_array[3] = 16'd0;
-                min_array[4] = 16'd0;
-                min_array[5] = 16'd0; 
-
-                flick_pos[0] = 16'd0;
-                flick_pos[1] = 16'b0000000000111111;
-
-                final_index = 3'd6;
-
-                next_LED = 16'd0;
-                next_index = 3'd0;
-                next_state = IDLE;
-            end
             IDLE: begin
                 next_LED = 16'd0;
                 next_index = 3'd0;
@@ -109,16 +102,9 @@ end
 //-----------------------------------------------------------------------//
 always@(posedge clk or negedge reset or posedge flick_trigger) begin
     if (!reset) begin
-        if (current_state == IDLE || current_state == GO_DOWN || current_state == GO_UP) begin
-            current_state <= IDLE;
-            LED <= 16'd0;
-            current_index <= 3'd0;
-        end
-        else begin
-            current_state <= BEGIN;
-            LED <= 16'd0;
-            current_index <= 3'd0;
-        end
+        current_state <= IDLE;
+        LED <= 16'd0;
+        current_index <= 3'd0;
     end
     else begin
         if (flick_trigger) begin
@@ -128,7 +114,7 @@ always@(posedge clk or negedge reset or posedge flick_trigger) begin
             end
             else begin
                 current_index <= current_index - 1;
-                LED <= LED;
+                // LED <= LED;
             end
             current_state <= GO_UP;
         end
